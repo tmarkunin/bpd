a3091d51e91f4a36ff7a0122ad7e2d639bd71195
Timur Markunin <tmarkunin@gmail.com>
2019-03-28 09:52:00 -0400

    use google terraform sample

--- TFproject/main.tf
+++ TFproject/main.tf
@@ -8,50 +8,42 @@ terraform {
   required_version = "> 0.8.0"
 }
 
-// Google Cloud provider
+// Configure the Google Cloud provider
 provider "google" {
-  version = "~> 1.5"
-  credentials = "${file("/home/tmarkunin/modern-ellipse-230911-32461a8aa8cc.json")}"
-  project     = "modern-ellipse-230911"
-  region      = "us-west1"
+ credentials = "${file("/home/tmarkunin/modern-ellipse-230911-32461a8aa8cc.json")}"
+ project     = "modern-ellipse-230911"
+ region      = "us-west1"
 }
 
-variable "unique_resource_name" {
-  description = "A unique name for the resource, required by GCE."
-  default = "TMarkunin test VM"
-}
-
-variable "machine_type" {
-  description = "The machine type to create."
-  default = "n1-standard-1"
-}
-
-variable "boot_disk" {
-  description = "The boot disk for the instance."
-  default = "centos-cloud/centos-7"
-}
-
-variable "zone" {
-  description = "The zone the resource should be created in."
-  default = "us-central1-a"
-}
 
 // Create a new compute engine resource
-resource "google_compute_instance" "default" {
-  name         = "${var.unique_resource_name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.zone}"
-  boot_disk {
-    initialize_params {
-      image = "${var.boot_disk}"
-    }
-  }
-  network_interface {
-    network = "default"
 
-    access_config {
-      // Ephemeral IP
-    }
-  }
+
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
 }
 
+// A single Google Cloud Engine instance
+resource "google_compute_instance" "default" {
+ name         = "flask-vm-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "us-west1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+// Make sure flask is installed on all new instances for later steps
+ metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
